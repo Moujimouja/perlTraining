a338c0202582f5d3f49bd096cdd78fbe6b616720
Moujimouja <slatergau@hotmail.com>
2014-07-16 13:56:52 +0200

    Rajout exercice 5

---
+++ Exercice5/exo.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings;
+
+my @t = (4,-5,7);
+@t = (@t, -2,3);
+
+print (join(',',@t)."\n");
+
+@t = (-0,-1,@t);
+$t[2] = 9;
+
+@t = sort {-$a <=> -$b} grep {$_ > 0} map{$_ * 2} @t;
+
+print (join(',',@t)."\n");
+
+
