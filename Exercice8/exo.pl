6b9029a2d565de8f35faf6ee400b5817a62a081a
Moujimouja <slatergau@hotmail.com>
2014-07-16 16:53:16 +0200

    MAJ Exercice 8

--- Exercice8/exo.pl
+++ Exercice8/exo.pl
@@ -3,4 +3,26 @@
 use strict;
 use warnings;
 
+my %uid;
+my @loginUid;
+open(my $fd, '<', 'passwd') or die ("Open :$!");
+
+my $ligne;
+
+while(defined($ligne = <$fd>)){
+	my ($login, undef, $uid) = split(/:/,$ligne);
+	#@loginUid = (@loginUid,$login,$uid);
+	$uid{$login} = $uid;	
+}
+
+#%uid = @loginUid;
+
+print  "\n UID HASH \n";
+
+foreach my $login (sort keys %uid){
+	print $login."\t".$uid{$login};
+        print "\n";
+}
+
+close($fd);
 
